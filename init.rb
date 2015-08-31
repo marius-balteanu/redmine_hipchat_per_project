3cd4b853d092f850ccf401ce9337e9a9fc2a69a2
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-08-31 12:39:09 +0300

    Damn those people who use tabs for indentation

--- init.rb
+++ init.rb
@@ -14,11 +14,11 @@ Redmine::Plugin.register :redmine_hipchat_per_project do
 
   requires_redmine version_or_higher: '3.1.0'
 
-	project_module :hipchat do
-		permission :view_hipchat_settings, { hipchat: :index }
-		permission :set_hipchat_settings, { hipchat: :save  }
-	end
-	settings default: {}
-	menu :project_menu, :hipchat, { controller: 'hipchat', action: 'index' },
+  project_module :hipchat do
+    permission :view_hipchat_settings, { hipchat: :index }
+    permission :set_hipchat_settings, { hipchat: :save  }
+  end
+  settings default: {}
+  menu :project_menu, :hipchat, { controller: 'hipchat', action: 'index' },
     caption: 'HipChat Settings', param: :identifier
 end
