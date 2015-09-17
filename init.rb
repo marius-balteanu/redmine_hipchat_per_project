476d249821e94155cb3d58e153087df478f2a8ba
Codrut Constantin Gusoi <codrut.gusoi@zitec.com>
2015-09-17 16:07:34 +0300

    Changed name and link

--- init.rb
+++ init.rb
@@ -1,16 +1,16 @@
 ActionDispatch::Callbacks.to_prepare do
   paths = '/lib/redmine_hipchat_per_project/{patches/*_patch,hooks/*_hook}.rb'
-  Dir.glob(File.dirname(__FILE__) + paths).each do |file|
+  Dir.glob(File.dirname(__FILE__) << paths).each do |file|
     require_dependency file
   end
 end
 
 Redmine::Plugin.register :redmine_hipchat_per_project do
-  name 'Redmine HipchatPerProject plugin'
+  name 'Hipchat Per Project'
   author 'Digital Natives'
   description 'Hipchat notifications for projects'
-  version '0.2.0'
-  url 'https://github.com/digitalnatives/redmine_hipchat_per_project'
+  version '0.2.1'
+  url 'https://github.com/sdwolf/redmine_hipchat_per_project'
 
   requires_redmine version_or_higher: '3.1.0'
 
